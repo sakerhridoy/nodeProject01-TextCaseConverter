3c624f62c188daae98c98e1c25c0960b44f19b8d
hridoy <sakerahmedhridoy@gmail.com>
2026-02-09 13:19:43 +0600

    error

--- server.js
+++ server.js
@@ -2,8 +2,22 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const handleUpload = require('./streams/uploadStream');
+const UpEmitter = require('./events/upEmitter');
 
 const PORT = 3000;
+
+UpEmitter.on('upProgress', (bytes, filename) => {
+  console.log(`Uploading ${filename}: ${bytes} bytes received`);
+});
+UpEmitter.on('upFinish', ( filename) => {
+  console.log(`${filename} Upload finished`);
+});
+UpEmitter.on('procFinish', filename => {
+  console.log(`${filename} Processing finished`);
+});
+UpEmitter.on('upError',(err, filename) => {
+  console.log(`${filename} Upload error occurred: ${err}`);
+});
 
 const server = http.createServer((req, res) => {
   if (req.method === 'POST' && req.url.startsWith('/upload')) {
